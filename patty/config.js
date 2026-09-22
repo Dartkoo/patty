9d22b8635fd25fd580381ab07e62ec9345452cde
Dartkoo <119898079+Dartkoo@users.noreply.github.com>
2026-09-22 05:29:19 -0600

    Add files via upload

---
+++ patty/config.js
@@ -0,0 +1,5 @@
+// Configuración de Patty Fotos & Videos
+window.PATTY_CONFIG = {
+  supabaseUrl: 'https://krwyutuovmphmleseqfz.supabase.co',
+  supabaseAnonKey: 'sb_publishable_nRVulDQksAau2nXO-j72mQ_JtYOK5Me'
+};
